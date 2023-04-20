6f524d19396810466acdc0e8e95b3a59ff176536
Patryk Czekaj <patryk13290@gmail.com>
2023-04-21 01:32:00 +0200

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    [SerializeField] private bool isGrounded;
+
+    private float moveInput;
+
+    [SerializeField] private LayerMask groundMask;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y - 0.5f), 0.2f, groundMask);
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        
+    }
+}
